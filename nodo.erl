d0d54ddcbe68a7a8c843c0c5590548977ed867dc
maph420 <juani.bertoni73@gmail.com>
2025-06-28 17:04:22 -0300

    Initial commit for part 3

---
+++ nodo.erl
@@ -0,0 +1,248 @@
+-module(nodo).
+-export([init/0, read_from_shared_folder/0, pprint/1, download_file/2, register/0, shell/0, comm_handler/1]).
+-include("config.hrl").
+
+% USAGE:
+% c(server).
+% server:server().
+
+% nc localhost 12345 (o alternativamente compilar nodo y descargar desde la cli)
+
+% Genera un nombre aleatorio de tamaño Sz, con mayusculas, minusculas y digitos
+name_generator(Sz) ->
+    Alphabet = lists:append([lists:seq(65,90), lists:seq(48,57), lists:seq(97,122)]),
+    N = length(Alphabet),
+    lists:map(fun(_) -> lists:nth(rand:uniform(N), Alphabet)
+        end,
+        lists:seq(1, Sz)
+    ).
+
+% Imprime de manera indexada y tabulada los elementos de una lista
+pprint(List) ->
+    IndList = lists:zip(lists:seq(1, length(List)), List),
+    lists:foreach(fun({Ind, X}) -> io:format("~p. ~p ~n", [Ind, X]) end, IndList).
+
+% Crea las carpetas del nodo si no están creadas.
+check_dirs() ->
+    case filelib:is_dir(?SHARED_PATH) of
+        true ->
+            io:format("La carpeta compartida esta presente ~n");
+        false ->
+            file:make_dir("compartida"),
+            io:format("Se creo la carpeta compartida~n")
+    end,
+    case filelib:is_dir(?DOWNLOADS_PATH) of
+        true ->
+            io:format("La carpeta descargas esta presente ~n");
+        false ->
+            file:make_dir("descargas"),
+            io:format("Se creo la carpeta descargas~n")
+    end,
+    ok.
+
+% Leer del directorio compartido, y retornar una lista con los archivos en él
+read_from_shared_folder() ->
+    case file:list_dir(?SHARED_PATH) of
+        {ok, RawFileNames} ->
+            % excluye por ejemplo links simbolicos, carpetas, sockets, pipes, etc
+            % el join es porque is_regular toma rutas, no nombres de archivo "sueltos"
+            [F || F <- RawFileNames, filelib:is_regular(filename:join(?SHARED_PATH , F))];
+        {error, Reason} ->
+            io:format("Error leyendo directorio ~p: ~p ~n", [?SHARED_PATH], [Reason]),
+            [] 
+    end.
+
+% Metodo que registra al nodo en la red
+register() ->
+    io:format("Registrar al nodo en ~p ~n", [?REG_PATH]).
+
+% Inicia la shell interactiva para el cliente
+shell() ->
+    io:format("linea de comandos~n"),
+    shell_loop(),
+    ok.
+
+% Funcion auxiliar usada por la shell interactiva
+shell_loop() ->
+    io:format("~s> ", [?SHELLNAME]),
+    case io:get_line("") of
+        eof ->
+            io:format("nos vimos~n");
+        {error, ErrorDescription} ->
+            io:format("Error: ~p ~n", [ErrorDescription]),
+            shell_loop();
+        Line ->
+            % pasarle la linea, quitandole el enter
+            if
+                length(Line) > 1 -> comm_handler(hd(string:tokens(Line, "\n")));
+                true -> ok
+            end,
+            shell_loop()
+    end.
+
+% Evaluar comandos
+comm_handler(Input) ->
+    Args = string:tokens(Input, " "),
+    case Args of
+        ["salir"] ->
+            io:format("me las piro vampiro~n"),
+            halt();
+        ["id_nodo"] ->
+            io:format("~p ~n", [self()]);
+        ["listar_mis_archivos"] ->
+            pprint(read_from_shared_folder());
+        ["DOWNLOAD_REQUEST", FileName, NodeIdStr] ->
+            io:format("Intentando conectarse a ~p para descargar ~s...~n", [NodeIdStr, FileName]),
+            case gen_tcp:connect(NodeIdStr, ?PORT, [binary, {packet, 0}, {active, false}]) of
+                {error, Reason} ->
+                    io:format("Error al conectar con el nodo: ~p~n", [Reason]),
+                    error;
+                %% si se pudo conectar, descargar el archivo
+                {ok, ConnSock} ->
+                    io:format("Conectado a ~p~n", [ConnSock]),
+                    case download_file(FileName, ConnSock) of
+                        {error, closed} ->
+                            io:format("Conexion cerrada antes de completar la descarga~n");
+                        {error, not_found} ->
+                            io:format("Error: archivo no encontrado en el server~n");
+                        {error, empty_file} ->
+                            io:format("Error: el archivo ~s esta vacio~n", [FileName]);
+                        ok ->
+                            gen_tcp:close(ConnSock),
+                            io:format("Archivo descargado! ~n")
+                    end
+            end;
+        ["help"] ->
+            pprint(?SHELL_COMMS);
+        _ ->
+            io:format("Comando desconocido. 'help' para ver los comandos disponibles y su uso. ~n")
+    end.
+
+% Descarga un archivo del servidor, dado su nombre y el socket de la conexion
+download_file(FileName, Sock) ->
+        io:format("Descargando archivo ~s...~n", [FileName]),
+        Req = "DOWNLOAD_REQUEST " ++ FileName ++ "\r\n",
+
+        io:format("Enviando request: ~s~n", [Req]),
+        ok = gen_tcp:send(Sock, Req),
+
+        % esperar a recibir la respuesta del servidor, cuando recibo leo el primer byte
+        case gen_tcp:recv(Sock, 1) of
+        {error, closed} ->
+            gen_tcp:close(Sock),
+            {error, closed};
+
+        {ok, <<Code:8>>} when Code =/= ?OK_CODE ->
+            gen_tcp:close(Sock),
+            {error, not_found};
+
+        {ok, <<?OK_CODE:8>>} ->
+            case gen_tcp:recv(Sock, + 4) of
+                {error, closed} ->
+                    {error, closed};
+                {ok, <<FileSize:32/big-unsigned-integer>>} ->
+                    case FileSize < ?MAX_SINGLE_FILE_SIZE of
+                        true  ->
+                            case FileSize > 0 of
+                                false ->
+                                    io:format("El archivo ~p no tiene contenido, no se enviara~n", [FileName]),
+                                    % aca se manda notfound, aunque en realidad el error es otro
+                                    gen_tcp:send(Sock, <<(?NOTFOUND_CODE):8>>),
+                                    {error, empty_file};
+                                true ->
+                                    io:format("Archivo chico (~p B), leo de una~n", [FileSize]),
+                                    read_direct(Sock, FileSize, FileName)
+                            end;
+                        false ->
+                            case gen_tcp:recv(Sock, 4) of
+                                {error, closed} ->
+                                    {error, closed};
+                               
+                                {ok, <<ChunkSize:32>>} -> % recibir el chunk size (no se usa, se podria prefijar con '_')
+                                    io:format("Archivo grande (~p B), leer de a chunks de ~p B ~n", [FileSize, ChunkSize]),
+                                    read_chunked(Sock, FileSize, FileName)
+                            end
+                    end
+            end
+        end.
+
+% Metodo para leer el archivo completo de una, sin chunks
+read_direct(Sock, FileSize, Nom) ->
+    case gen_tcp:recv(Sock, FileSize) of
+        {error, closed} ->
+            io:format("Conexion cerrada antes de recibir el archivo~n"),
+            {error, closed};
+        {ok, BinData} -> 
+            FullPath = filename:join(?DOWNLOADS_PATH, Nom),
+            ok = file:write_file(FullPath, BinData),
+            io:format("Archivo guardado en: ~p~n", [FullPath]),
+            ok
+    end.
+    
+% Metodo para leer el archivo en partes, cuando el tamaño es mayor al umbral
+read_chunked(Sock, FileSize, Nom) ->
+    % ya lei los primeros 1 + 4 + 4 bytes, ahora lo que resta es del archivo
+    % Abrir archivo
+    FullPath = filename:join(?DOWNLOADS_PATH, Nom),
+    case file:open(FullPath, [write, binary]) of
+        {error, Reason} ->
+            io:format("Error al abrir el archivo ~p: ~p~n", [FullPath, Reason]),
+            {error, open_file_failed};
+        {ok, File} ->
+            read_chunks_loop(Sock, File, FileSize),
+            file:close(File),
+            gen_tcp:close(Sock),
+            ok
+    end.
+
+read_chunks_loop(_Sock, _File, 0) ->
+    ok;
+read_chunks_loop(Sock, File, Rem) when Rem > 0 ->
+    io:format("Quedan ~p bytes por recibir~n", [Rem]),
+
+    case gen_tcp:recv(Sock, 1 + 2 + 4) of
+        {error, closed} ->
+            io:format("Conexion cerrada antes de recibir el chunk~n"),
+            {error, closed};
+        {ok, <<ChunkCode:8, _ChunkInd:16, CurrChunkSize:32/big-unsigned-integer>>} ->
+            handle_chunk(Sock, File, Rem, ChunkCode, CurrChunkSize)
+    end.
+
+
+handle_chunk(Sock, File, Rem, ChunkCode, CurrChunkSize) ->
+    case ChunkCode == ?CHUNK_CODE of
+        true ->
+            case gen_tcp:recv(Sock, CurrChunkSize) of
+                {error, closed} ->
+                    io:format("Conexion cerrada antes de recibir el chunk~n"),
+                    {error, closed};
+                {ok, Bin} ->
+                    io:format("Recibido chunk de ~p bytes~n", [CurrChunkSize]),
+                    % obs la diferencia con write_file: que no requeria que el archivo estuviera abierto 
+                    % (lo hacia manualmente, pero hacer esto recursivamente es costoso)
+                    ok = file:write(File, Bin),
+                    read_chunks_loop(Sock, File, Rem - CurrChunkSize)
+                end;
+        false ->
+            io:format("Error: código de chunk inválido: ~p~n", [ChunkCode]),
+            {error, invalid_chunk_code}
+    end.
+
+init() ->
+    io:format("Inicializa nodo ~n"),
+    check_dirs(),
+    _Shr = read_from_shared_folder(),
+    _Discovered = [],
+    register(),
+    io:format("Archivos compartidos: ~p ~n", [?TEST_NAMES]),
+    
+    Name = name_generator(?NODE_NAME_LENGTH),
+    io:format("Nombre del nodo: ~p ~n", [Name]),
+
+    %% TODO: registrar el nodo a traves de UDP
+
+    % probablemente convenga dejar el server registrado haciendole spawn con otro proceso
+    % server:server(),
+
+    shell(),
+    ok.
---
+++ server.erl
@@ -0,0 +1,178 @@
+-module(server).
+-export([server/0]).
+-include("config.hrl").
+
+% USAGE:
+% c(server).
+% server:server().
+
+% nc localhost 12345 (o alternativamente compilar nodo y descargar desde la cli)
+
+%% buscar el archivo en la carpeta compartida, si esta retornar su ruta completa y su tamaño (en bytes)
+%% si no esta, retornar error
+file_lookup(FileName) ->
+    FullSharedPath = filename:join(?SHARED_PATH, FileName),
+    FullDownPath = filename:join(?DOWNLOADS_PATH, FileName),
+    case filelib:is_regular(FullSharedPath) of
+        true ->
+            {ok, FullSharedPath, filelib:file_size(FullSharedPath)};
+        false ->
+            case filelib:is_regular(FullDownPath) of
+                true ->
+                    {ok, FullDownPath, filelib:file_size(FullDownPath)};
+                false ->
+                    {error, not_found}
+            end
+    end.
+
+%% Inicia el servidor, escuchando en el puerto definido en config.hrl (espera activa)
+server() ->
+    io:fwrite("Hola soy ~p~n", [self()]),
+    {ok, ListenSocket} = gen_tcp:listen(?PORT, [{reuseaddr, true}, {active, true}]),
+    io:fwrite("Escuchando peticiones ~n"),
+    wait_connect(ListenSocket).
+
+% Método de escucha del listener, al llegar conexioens nuevas las pasa a un proceso hijo para que las maneje.
+wait_connect(ListenSocket) ->
+    io:fwrite("~p se encarga de handlear a los clientes ~n", [self()]),
+    {ok, Socket} = gen_tcp:accept(ListenSocket),
+    io:fwrite("Conexion desde ~p aceptada ~n", [Socket]),
+
+    % Le transfiero "la propiedad" del socket al encargado de handlearlo. Sino, este nunca recibira los mensajes tcp
+    gen_tcp:controlling_process(Socket, spawn (fun() -> handle_request(Socket) end)),
+    wait_connect(ListenSocket).
+
+% Envia el archivo (cuyo tamaño supera al umbral) en partes
+send_chunks(Sock, BinData, FileSize, CantChunk, N) ->
+    io:fwrite("iteracion ~p de ~p chunks~n", [N, CantChunk]),
+    case N == (CantChunk - 1) of
+        true -> 
+            io:fwrite("Enviando chunk ~p de ~p bytes~n", [N, ?MAX_SINGLE_FILE_SIZE]),
+            ChunkSize = FileSize - (?MAX_SINGLE_FILE_SIZE * (N)),
+            ChunkData = binary:part(BinData, N * (?MAX_SINGLE_FILE_SIZE), ChunkSize),
+            % siempre mandamos chunks de tamaño MAX_SINGLE_FILE_SIZE, excepto el ultimo que puede ser menor
+            Chunk = <<
+                (?CHUNK_CODE):8,
+                N:16,
+                ChunkSize:32/big-unsigned-integer,
+                ChunkData/binary
+            >>,
+            gen_tcp:send(Sock, Chunk),
+            ok;
+        false ->
+            io:fwrite("Enviando chunk ~p de ~p bytes~n", [N, ?MAX_SINGLE_FILE_SIZE]),
+            ChunkData = binary:part(BinData, N * (?MAX_SINGLE_FILE_SIZE), ?MAX_SINGLE_FILE_SIZE),
+            Chunk = <<
+                (?CHUNK_CODE):8,
+                N:16,
+                (?MAX_SINGLE_FILE_SIZE):32/big-unsigned-integer,
+                ChunkData/binary
+            >>,
+
+            case gen_tcp:send(Sock, Chunk) of
+                ok -> 
+                    io:fwrite("Chunk enviado! ~n"),
+                    ok;
+                {error, Reason} ->
+                    io:fwrite("Error al enviar el chunk: ~p~n", [Reason]),
+                    error
+            end,
+            send_chunks(Sock, BinData, FileSize, CantChunk, N + 1)
+    end.
+
+% Envía el archivo encontrado al cliente, si es menor al umbral lo envía de una, si es mayor lo envía en partes
+send_found_file(Socket, FullPath, FileSize) ->
+
+    case file:read_file(FullPath) of
+        {error, Reason} ->
+            io:format("Error al leer el archivo: ~p~n", [Reason]),
+            gen_tcp:send(Socket, <<(?NOTFOUND_CODE):8>>),
+            error;
+        {ok, BinData} ->
+            case FileSize < ?MAX_SINGLE_FILE_SIZE of
+                true ->
+                    case FileSize > 0 of
+                        false ->
+                            io:format("El archivo ~p no tiene contenido, no se enviara~n", [FullPath]),
+                            % aca se manda notfound, aunque en realidad el error es otro
+                            gen_tcp:send(Socket, <<(?NOTFOUND_CODE):8>>),
+                            error;
+                        true ->
+                             % Si el archivo es menor al umbral, lo mando de una
+                            Packet = <<
+                                (?OK_CODE):8,
+                                FileSize:32/big-unsigned-integer,
+                                BinData/binary
+                            >>,
+                            io:format("Paquete definido: ~p~n", [Packet]),
+
+                            case gen_tcp:send(Socket, Packet) of
+                                ok -> 
+                                    io:format("Archivo enviado! ~n"),
+                                    ok;
+                                {error, Reason} ->
+                                    io:format("Error al enviar el archivo: ~p~n", [Reason]),
+                                    error
+                            end,
+                            io:format("Archivo enviado! ~n")
+                    end;
+                
+                false ->
+                    % Si el archivo es mayor al umbral, lo mando en partes
+                    Packet = <<
+                        (?OK_CODE):8,
+                        FileSize:32/big-unsigned-integer,
+                        (?MAX_SINGLE_FILE_SIZE):32/big-unsigned-integer
+                    >>,
+
+                    case gen_tcp:send(Socket, Packet) of
+                        ok -> 
+                            io:format("Paquete enviado! ~n"),
+                            ok;
+                        {error, Reason} ->
+                            io:format("Error al enviar el paquete: ~p~n", [Reason]),
+                            error
+                    end,
+
+                    CantChunks = ceil(FileSize / (?MAX_SINGLE_FILE_SIZE)),
+                    io:format("Cant chunks: ~p~n", [CantChunks]),
+                    send_chunks(Socket, BinData, FileSize, CantChunks, 0)
+            end
+    end.
+
+% Maneja las conexiones entrantes, recibe los mensajes y responde a las peticiones
+handle_request(Socket) ->
+    io:fwrite("~p se encarga de handlear la conexion ~n", [self()]),
+    receive
+        {tcp, Cli, Data} ->
+            Args = string:tokens(Data, " \n\r"),
+            case Args of
+                ["DOWNLOAD_REQUEST", FileName] ->
+                    io:format("Download req: ~p ~n", [FileName]),
+                    case file_lookup(FileName) of
+                        {ok, FullPath, FileSize} ->
+                            io:format("Archivo encontrado! ~n"),
+                            send_found_file(Socket, FullPath, FileSize);
+        
+                        {error, not_found} ->
+                            io:format("Archivo no encontrado: ~p ~n", [FileName]),
+                            Packet = <<(?NOTFOUND_CODE):8>>,
+                            gen_tcp:send(Socket, Packet),
+                            error
+                    end,
+                    handle_request(Socket);
+                ["QUIT"] ->
+                    io:format("User quits~n"),
+                    gen_tcp:close(Socket);
+                _Otherwise ->
+                    gen_tcp:send(Cli, "ERROR: Comando desconocido\r\n"),
+                    handle_request(Socket)
+            end;
+        {tcp_closed, Cli} ->
+            io:format("Cliente ~p desconectado.~n", [Cli]),
+            ok;
+
+        _OtherMsg -> 
+            io:format("Mensaje inesperado: ~p~n", [_OtherMsg]),
+            handle_request(Socket)
+    end.
