54de9b13ab12e26393c9522c1e11672924cd0c29
TomasSegovia <158337927+TomasSegovia@users.noreply.github.com>
2025-06-30 22:56:16 -0300

    Comentado de server_erl + fix de handle_request

--- server.erl
+++ server.erl
@@ -1,26 +1,25 @@
-%TODO: Explain what each file does
 -module(server).
 -export([server/0]).
 -include("config.hrl").
 
-%% Inicia el servidor, escuchando en el puerto definido en config.hrl (espera activa)
+% Starts the server, listening to the defined port used in config.hrl (active wait)
 server() ->
     io:fwrite("Hola soy ~p~n", [self()]),
     {ok, ListenSocket} = gen_tcp:listen(?PORT, [{reuseaddr, true}, {active, true}]),
     io:fwrite("Escuchando peticiones ~n"),
     wait_connect(ListenSocket).
 
-% Método de escucha del listener, al llegar conexioens nuevas las pasa a un proceso hijo para que las maneje.
+% Listening method, when new connections come it creates a child proccess to handle it.
 wait_connect(ListenSocket) ->
     io:fwrite("~p se encarga de handlear a los clientes ~n", [self()]),
     {ok, Socket} = gen_tcp:accept(ListenSocket),
     io:fwrite("Conexion desde ~p aceptada ~n", [Socket]),
 
-    % Le transfiero "la propiedad" del socket al encargado de handlearlo. Sino, este nunca recibira los mensajes tcp
+    % Transfers the "property" of the socket to the procces that is in charge of it. If not, this never recieves tcp messages
     gen_tcp:controlling_process(Socket, spawn (fun() -> handle_request(Socket) end)),
     wait_connect(ListenSocket).
 
-% Maneja las conexiones entrantes, recibe los mensajes y responde a las peticiones
+% Handles incoming connections, gets the messages and answers queries
 handle_request(Socket) ->
     io:fwrite("~p se encarga de handlear la conexion ~n", [self()]),
     receive
@@ -36,7 +35,7 @@ handle_request(Socket) ->
 
                             io:format("Archivo encontrado! ~n"),
                             spawn(fun() -> downloader:send_found_file(Socket, FullPath, FileSize) end);
-        
+
                         #fileLookupError{reason = _} ->
                             io:format("Archivo no encontrado: ~p ~n", [FileName]),
                             Packet = <<(?NOTFOUND_CODE):8>>,
@@ -49,6 +48,7 @@ handle_request(Socket) ->
                     gen_tcp:close(Socket);
                 ["SEARCH_REQUEST", _, FileName] ->
                     file_gen:search_response(Socket, FileName);
+                    handle_request(Socket);
                 _Otherwise ->
                     gen_tcp:send(Cli, "ERROR: Comando desconocido\r\n"),
                     handle_request(Socket)
