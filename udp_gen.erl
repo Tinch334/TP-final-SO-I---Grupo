7e48e279e184f4b0e32238b2556a716728cd92f1
TomasSegovia <158337927+TomasSegovia@users.noreply.github.com>
2025-06-30 23:09:09 -0300

    Comentado de udp_gen

--- udp_gen.erl
+++ udp_gen.erl
@@ -1,10 +1,9 @@
-%TODO: Explain what each file does
 -module(udp_gen).
 -export([gen_udp_init/0, hello_sender_init/2, hello_sender/2, namereq_sender/2, udp_discover_listener/1, handle_udp_req/3, choose_name/2]).
 
 -include("config.hrl").
 
-% listen for incoming UDP requests
+% Listen for incoming UDP requests
 udp_discover_listener(Sock) ->
     case gen_udp:recv(Sock, 0) of
         {ok, {Ip, _Port, Str}} ->
@@ -15,15 +14,14 @@ udp_discover_listener(Sock) ->
             udp_discover_listener(Sock)
     end.
 
-% handle the request received from UDP socket
+% Handle the request received from UDP socket
 handle_udp_req(Str, IpFrom, Sock) ->
     case string:tokens(Str, " \n") of
         ["NAME_REQUEST", IdStr] ->
             io:format("NAME_REQUEST from ~p with id: ~p~n", [IpFrom, IdStr]),
             Id = IdStr,
             NV = node:get_node_value(),
-            % TODO: verificar si: El mismo id ya fue solicitado en una NAME_REQUEST enviada por este node anteriormente.
-            case Id =/= NV of
+            case Id =/= NV of % Checks if the new node has my name
                 true ->
                     case utils:id_in_registry(Id) of
                         true ->
@@ -34,7 +32,7 @@ handle_udp_req(Str, IpFrom, Sock) ->
                     end;
                 false ->
                     io:format("NAME_REQUEST with my own id!, invalidate.~n"),
-                    gen_udp:send(Sock, IpFrom, (?UDP_SOCKET), string:join(["INVALID_NAME", IdStr, "\n"], " "))
+                    Sock ! #udpSend{addr = IpFrom, port = ?UDP_SOCKET, msg = string:join(["INVALID_NAME", IdStr, "\n"], " ")}
             end,
             udp_discover_listener(Sock);
 
@@ -49,29 +47,30 @@ handle_udp_req(Str, IpFrom, Sock) ->
             udp_discover_listener(Sock)
     end.
 
-% follows the protocol to choose a unique name for the node. precond: an active UDP connection is open
+% Follows the protocol to choose a unique name for the node, an active UDP connection has to be open for this to work
 choose_name(Sock, Timeout) ->
     NameChoice = name_generator(?NODE_NAME_LENGTH),
-    namereq_sender(NameChoice, Sock), % send name request
+    namereq_sender(NameChoice, Sock), % Send name request
     %io:format("Sending name: ~p~n", [NameChoice]),
     wait_for_invalid_name(Sock, NameChoice, Timeout).
 
+% Waits for a random amount of time between 2 and 10 seconds, when the time passes it assumes that no one has that name
 wait_for_invalid_name(Sock, NameChoice, RemainingTime) ->
     case (RemainingTime =< 0) of
         true -> 
             %io:format("Time's up. Name is accepted. ~p~n", [NameChoice]),
             NameChoice;
         false ->
-            Start = erlang:monotonic_time(millisecond), % start counting time
+            Start = erlang:monotonic_time(millisecond), % Starts counting time
             case gen_udp:recv(Sock, 0, RemainingTime) of
                 {ok, {_Addr, _Port, Data}} ->
                     case Data of
                         {"INVALID_NAME", _ID} ->
                             %io:format("Name ~p already in use!~n", [NameChoice]),
-                            timer:sleep(rand:uniform(8000) + 2000),  % wait a random time between 2s-10s and retry
+                            timer:sleep(rand:uniform(8000) + 2000),  % Waits a random time between 2s-10s and retries
                             choose_name(Sock, 10000); 
                         _Other ->
-                            Elapsed = erlang:monotonic_time(millisecond) - Start, % stop counting, wait again but less
+                            Elapsed = erlang:monotonic_time(millisecond) - Start, % Stops counting, waiting for a shorter amount of time
                             NewRemaining = RemainingTime - Elapsed,
                             wait_for_invalid_name(Sock, NameChoice, NewRemaining)
                     end;
@@ -84,7 +83,7 @@ wait_for_invalid_name(Sock, NameChoice, RemainingTime) ->
             end
     end.
 
-% Genera un nombre aleatorio de tamaño Sz, con mayusculas, minusculas y digitos
+% Randomly generates a name with Sz size, according to the specification details (it can have mayus, minus and digits)
 name_generator(Sz) ->
     Alphabet = lists:append([lists:seq(65,90), lists:seq(48,57), lists:seq(97,122)]),
     N = length(Alphabet),
@@ -100,10 +99,10 @@ gen_udp_init() ->
             io:format("Error creating UDP socket: [~p] ~n", [Reason]);
         {ok, UDPConnSock} ->
             %io:format("UDP sucessfully created~n"),
-            NodeName = choose_name(UDPConnSock, ?NAME_WAIT), % get unique name for the node
-            register(?NODE_NAME_HOLDER, spawn(fun() -> node:name_holder_loop(NodeName) end)), % launch the name holder process
-            register(?UDP_SENDER_ID, spawn(fun() -> udp_gen:hello_sender_init(NodeName, UDPConnSock), receive after infinity -> ok end  end)) , % periodically send HELLO
-            register(?UDP_RECEIVER_ID, spawn(fun() -> udp_discover_listener(UDPConnSock) end)), % listen for incoming UDP requests
+            NodeName = choose_name(UDPConnSock, ?NAME_WAIT), % Gets a unique name for the node
+            register(?NODE_NAME_HOLDER, spawn(fun() -> node:name_holder_loop(NodeName) end)), % Launches the name holder process
+            register(?UDP_SENDER_ID, spawn(fun() -> udp_gen:hello_sender_init(NodeName, UDPConnSock), receive after infinity -> ok end  end)) , % Periodically sends HELLO
+            register(?UDP_RECEIVER_ID, spawn(fun() -> udp_discover_listener(UDPConnSock) end)), % Listens for incoming UDP requests
             io:format("Nombre del node: ~p~n", [node:get_node_value()])
     end.
 
